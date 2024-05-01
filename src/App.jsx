f3bb4aab9a2b4e245ac655651ef18fd4836d5807
Yoho <yohovanivargas@gmail.com>
2024-05-01 13:36:42 -0600

    Musica y mensaje, corrección de tamaño en imgs

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,31 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from 'react-bootstrap/Container';
 import globos from './assets/globos.png'
 import princesa from './assets/princesa.png'
+import cancion from './assets/audio/Te_Esperaba.mp3'
+import Button from 'react-bootstrap/Button';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [isPlaying, setIsPlaying] = useState(true); // Estado para controlar la reproducción del audio
+  const togglePlay = () => {
+    setIsPlaying(!isPlaying); // Cambia el estado de reproducción al hacer clic en el botón de reproducción/pausa
+  };
 
   return (
     <>
+    <br/>
     <img src={globos} className='globos img-fluid'/>
       <Container className='margen'>
-        <img src={princesa} className='princesa'/>
+        <img src={princesa} className='princesa'/><br/>
+        <h2 style={{ fontFamily: 'MiFuente' }}>La dulce espera esta por terminar</h2><br/>
+
+        <div>
+          <audio src={cancion} autoPlay={isPlaying}/> {/* Utiliza el atributo autoPlay para iniciar la reproducción automáticamente si isPlaying es true */}
+          <Button  onClick={togglePlay}>{isPlaying ? 'Pausar' : 'Reproducir'}</Button> {/* Botón para pausar/reproducir */}
+        </div>
     </Container>
     </>
   )
