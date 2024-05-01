94632fbf0ed6d3512f50fa71455cadc398a05681
Yoho <yohovanivargas@gmail.com>
2024-05-01 12:05:19 -0600

    se completa primera etapa del borrador

--- src/App.jsx
+++ src/App.jsx
@@ -2,32 +2,20 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import globos from './assets/globos.png'
+import princesa from './assets/princesa.png'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <img src={globos} className='globos img-fluid'/>
+      <Container className='margen'>
+        <img src={princesa} className='princesa'/>
+    </Container>
     </>
   )
 }
